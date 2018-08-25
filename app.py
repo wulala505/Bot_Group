7826f27833033083f6aa6790f04c3ff13d950336
Pinchen Huang <wulala505@gmail.com>
2018-08-25 15:25:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,11 +18,33 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+# for push
+from linebot.models import *
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('naKmf9eBBxHxc1SI0EjRRHZvJMvOqHSIithTj0GOa4A86daBN5zhdAOYy/ogkdDR3/xvXzwXzZndfkva6ymwDzhy6u+EKWW8CgzLkTh9IbvA/a6Fj3Zv++sWV3CmEbemc4xe3L+lFhMTocZdV9yNtwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('37a36ae96eb9730856b07ed5e5edc8a7')
 
+# for push
+message = TemplateSendMessage(
+    alt_text='你覺得這個機器人方便嗎？',
+    template=ConfirmTemplate(
+        text='你覺得這個機器人方便嗎？',
+        actions=[
+            MessageTemplateAction(
+                label='很棒！',
+                text='ＧＯＯＤ'
+            ),
+            MessageTemplateAction(
+                label='有待加強',
+                text='波比好棒棒'
+            )
+        ]
+    )
+)
+line_bot_api.push_message(userID, message)
+ 
 
 
 @app.route("/callback", methods=['POST'])
